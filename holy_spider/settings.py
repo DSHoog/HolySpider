5507f64bce7bc0814370994816f10d3289a3152c
Radar <moddedisaacfan@gmail.com>
2023-05-08 14:29:03 -0400

    Fixes to code and changes to location of files.

--- holy_spider/settings.py
+++ holy_spider/settings.py
@@ -6,6 +6,13 @@ BOT_NAME = 'the_holy_spider'
 SPIDER_MODULES = ['holySpider.holy_spider_scraper']
 NEWSPIDER_MODULE = 'holySpider.holy_spider_scraper'
 
+# ScraOps API Key
+SCRAPEOPS_API_KEY = 'cbe2aba4-0561-45bd-9f9f-a223e227ca02'
+
+EXTENSIONS = {
+        'scrapeops_scrapy.extension.ScrapeOpsMonitor': 500, 
+        }
+
 # Set the user agent to identify as a web browser
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
 
